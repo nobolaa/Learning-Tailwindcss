a5eba5b690428bd9859f27264f7dddd32f0200af
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-19 23:45:56 +0200

    Added configuration to have 16 columns in each row

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ module.exports = {
     container: {
       center: true,
     },
-    extend: {},
-  },
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '16': 'repeat(16, minmax(0, 1fr))',
+
+        // Complex site-specific column configuration
+        'footer': '200px minmax(900px, 1fr) 100px',
+      },
+    },
   plugins: [],
+  }
 }

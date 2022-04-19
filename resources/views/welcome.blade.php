994e5e1fe6bc67fd3ad05af57fdd503b33c6bf05
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 00:39:01 +0200

    Using different letter spacing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,8 @@
 </head>
 <body>
     <div class="container">
-        <p class="font-josefin font-hairline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam cupiditate corrupti doloremque! Temporibus voluptas dolor quia alias vero expedita at! Consequuntur, earum doloribus quo quas libero ducimus facilis laboriosam odio.</p>
-        <p class="font-josefin font-light">Modi totam tenetur accusamus inventore laboriosam nulla distinctio consequuntur non provident aliquam, quidem quis assumenda commodi. Nemo quibusdam quia, repellendus non dolores voluptatum assumenda maxime, earum maiores ad in commodi?</p>
+        <p class="font-josefin font-hairline tracking-widest">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam cupiditate corrupti doloremque! Temporibus voluptas dolor quia alias vero expedita at! Consequuntur, earum doloribus quo quas libero ducimus facilis laboriosam odio.</p>
+        <p class="font-josefin font-light tracking-tighter">Modi totam tenetur accusamus inventore laboriosam nulla distinctio consequuntur non provident aliquam, quidem quis assumenda commodi. Nemo quibusdam quia, repellendus non dolores voluptatum assumenda maxime, earum maiores ad in commodi?</p>
         <p class="font-josefin font-semibold">Neque blanditiis illo facilis excepturi consequuntur. Dolore fuga delectus laborum in aut beatae iste dolores, consequatur hic soluta quibusdam. Rem quia ea placeat eum, vel molestiae blanditiis adipisci illum reiciendis!</p>
         <p class="font-josefin font-black">Accusamus a nisi veniam id voluptates alias. Itaque, perferendis? Facilis repellat tenetur similique aspernatur esse odio modi facere consequuntur, ducimus alias tempore deserunt harum? Ad blanditiis error eius accusamus cupiditate?</p>
     </div>

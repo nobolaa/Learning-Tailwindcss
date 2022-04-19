c6a38fd197ff2f7af4458494b25cf2540a2fb065
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 00:49:33 +0200

    Playing with:     - List icons     - List padding     - Text colour     - Text opacity     - Text hover     - Text underline and linetrough     - Text uppercase, lowercase and capitalize     - Text align (left, center, right) on different screen sizes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,15 @@
 </head>
 <body>
     <div class="container">
-        <p class="font-josefin font-hairline tracking-widest">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam cupiditate corrupti doloremque! Temporibus voluptas dolor quia alias vero expedita at! Consequuntur, earum doloribus quo quas libero ducimus facilis laboriosam odio.</p>
-        <p class="font-josefin font-light tracking-tighter">Modi totam tenetur accusamus inventore laboriosam nulla distinctio consequuntur non provident aliquam, quidem quis assumenda commodi. Nemo quibusdam quia, repellendus non dolores voluptatum assumenda maxime, earum maiores ad in commodi?</p>
-        <p class="font-josefin font-semibold">Neque blanditiis illo facilis excepturi consequuntur. Dolore fuga delectus laborum in aut beatae iste dolores, consequatur hic soluta quibusdam. Rem quia ea placeat eum, vel molestiae blanditiis adipisci illum reiciendis!</p>
-        <p class="font-josefin font-black">Accusamus a nisi veniam id voluptates alias. Itaque, perferendis? Facilis repellat tenetur similique aspernatur esse odio modi facere consequuntur, ducimus alias tempore deserunt harum? Ad blanditiis error eius accusamus cupiditate?</p>
+        <h1 class="text-xl font-bold text-left md:text-center lg:text-right underline uppercase">Este es el titulo</h1>
+        <p class="text-red-700 hover:text-opacity-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, consectetur molestiae officiis, perferendis asperiores sapiente iusto ad suscipit autem quis sit eveniet odio quibusdam tempora delectus ducimus odit adipisci rem?</p>
+        <ul class="list-disc list-inside">
+            <li class="line-through">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus libero asperiores explicabo sit magni placeat provident hic itaque, voluptas dolores animi ipsam sequi laboriosam vero doloribus laborum autem nisi repellendus.</li>
+            <li>Accusamus quod dicta perferendis tenetur iure, inventore cumque maiores doloribus sapiente, minima expedita nostrum quis voluptatem natus. Nemo, porro deleniti natus maxime esse aspernatur illum. Corporis deserunt ex voluptas at.</li>
+            <li>Accusamus rerum dignissimos, at sed nisi dolorem deleniti sequi exercitationem incidunt eveniet quae ex accusantium perspiciatis, tenetur, odit laboriosam culpa. Excepturi aspernatur explicabo ut enim ipsum accusantium assumenda aperiam quaerat!</li>
+            <li class="capitalize">Tempore voluptatibus recusandae, doloribus perspiciatis sint dolores beatae perferendis fuga deserunt odio amet quis illum nesciunt sed sunt unde explicabo, temporibus delectus totam voluptatum voluptas quaerat ipsa blanditiis minus! Quam.</li>
+            <li class="lowercase">Recusandae autem eaque ea in cumque consequuntur aspernatur minima omnis. Voluptatum sequi dolor nesciunt unde rem, earum doloremque eaque hic commodi! A, quisquam neque consequatur consequuntur autem dolore? Velit, magni!</li>
+        </ul>
     </div>
 </body>
 </html>

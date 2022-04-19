93c158aa2296824e8ceac9a3ca175c1751c10dda
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 00:26:18 +0200

    Moved styles for h1, p and li components to resource/css/app.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,9 @@
 </head>
 <body>
     <div class="container">
-        <h1 class="font-sans text-6xl md:text-3xl font-bold mb-4">Este es un titulo de prueba</h1>
-        <p class="font-serif leading-relaxed mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt perferendis voluptates officia. Mollitia, iste ratione minus vero illo consequuntur quibusdam. Maxime eaque repellendus cupiditate, voluptatum tempora vel magnam rem sapiente.</p>
-        <ul class="font-mono text-sm italic">
+        <h1>Este es un titulo de prueba</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt perferendis voluptates officia. Mollitia, iste ratione minus vero illo consequuntur quibusdam. Maxime eaque repellendus cupiditate, voluptatum tempora vel magnam rem sapiente.</p>
+        <ul>
             <li>Elemento 1</li>
             <li>Elemento 2</li>
             <li>Elemento 3</li>

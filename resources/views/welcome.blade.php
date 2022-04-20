ef1be169e6b8530de008f24165b80bdd211cb00b
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 22:29:30 +0200

    Using float property for images and clear left/right/both for the text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,21 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container mt-4 bg-blue-600">
-        <div class="bg-gray-400 text-gray-700 text-center px-4 py-2 inline-block">1</div>
-        <div class="bg-gray-400 text-gray-700 text-center px-4 py-2 my-2 inline">2</div>
-        <div class="bg-gray-400 text-gray-700 text-center px-4 py-2 hidden">3</div>
+    <div class="container">
+
+        <img class="float-left w-1/5" src="{{ asset('img/tailwind2.png.') }}" alt="">
+        <img class="float-right w-1/3" src="{{ asset('img/tailwind-intellisense.jpg.') }}" alt="">
+
+        <p class="mb-2 clear-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus necessitatibus sit rerum, eveniet exercitationem porro quibusdam dignissimos iste perferendis deleniti. Molestiae ratione dolore veniam officiis, repudiandae debitis magni asperiores nostrum?</p>
+        <p class="mb-2">Natus itaque nihil ex, illum repellat assumenda est, atque consequatur, obcaecati a recusandae expedita soluta quidem unde voluptatum eveniet consequuntur! Placeat in cum doloribus incidunt veniam sint impedit, aperiam nobis?</p>
+        <p class="mb-2">Deleniti ratione dignissimos alias, asperiores repellendus eaque saepe ut ex similique officiis harum amet, iusto at quas perferendis eius totam quo ea mollitia, debitis impedit vero necessitatibus. Tenetur, neque autem.</p>
+        <p class="mb-2">Dolorem fugiat inventore recusandae dignissimos dolores praesentium, iste blanditiis, nemo maiores adipisci eum velit quos officia! Fugit delectus quaerat rerum recusandae nobis debitis dolores illum aspernatur eius, hic placeat quisquam?</p>
+        <p class="mb-2 clear-both">Consequuntur quasi perferendis nesciunt nihil facilis cupiditate vero assumenda deleniti voluptatem impedit quaerat nulla tempora, illo laudantium, quam esse dolor dignissimos veniam aspernatur quisquam fugiat delectus. Itaque aut vel molestiae.</p>
+        <p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus necessitatibus sit rerum, eveniet exercitationem porro quibusdam dignissimos iste perferendis deleniti. Molestiae ratione dolore veniam officiis, repudiandae debitis magni asperiores nostrum?</p>
+        <p class="mb-2">Natus itaque nihil ex, illum repellat assumenda est, atque consequatur, obcaecati a recusandae expedita soluta quidem unde voluptatum eveniet consequuntur! Placeat in cum doloribus incidunt veniam sint impedit, aperiam nobis?</p>
+        <p class="mb-2">Deleniti ratione dignissimos alias, asperiores repellendus eaque saepe ut ex similique officiis harum amet, iusto at quas perferendis eius totam quo ea mollitia, debitis impedit vero necessitatibus. Tenetur, neque autem.</p>
+        <p class="mb-2">Dolorem fugiat inventore recusandae dignissimos dolores praesentium, iste blanditiis, nemo maiores adipisci eum velit quos officia! Fugit delectus quaerat rerum recusandae nobis debitis dolores illum aspernatur eius, hic placeat quisquam?</p>
+        <p class="mb-2">Consequuntur quasi perferendis nesciunt nihil facilis cupiditate vero assumenda deleniti voluptatem impedit quaerat nulla tempora, illo laudantium, quam esse dolor dignissimos veniam aspernatur quisquam fugiat delectus. Itaque aut vel molestiae.</p>
     </div>  
 </body>
 </html>

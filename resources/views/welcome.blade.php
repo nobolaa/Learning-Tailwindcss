5a0fa9d3aecae68184e184c01aaef628262b43c0
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 17:36:27 +0200

    Using bg-fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         .imagen{
             background-image: url("{{asset('img/tailwind-intellisense.jpg')}}");
             height: 400px;
-            width: 400px;
+            width: 900px;
             margin: 0 auto;
         }
     </style>
@@ -20,7 +20,17 @@
     <div class="container">
         <h1 class="text-center text-3xl font-bold mb-3">Background</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta adipisci voluptatem pariatur at architecto, ratione saepe cumque dolores dolorum porro, consectetur illo necessitatibus, ipsa vitae possimus minima provident nobis recusandae.</p>
-        <div class="imagen bg-cover bg-center border-8 border-blue-600 border-dashed bg-clip-content p-4"></div>
+        <div class="imagen bg-fixed"></div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus pariatur quibusdam recusandae. Facilis consequuntur ipsum, magni autem asperiores mollitia natus, tenetur, iure eveniet repudiandae cumque vero unde dolore accusamus alias.</p>
+        <p>Consequuntur blanditiis corporis dicta officia quam, totam soluta commodi at, reiciendis culpa temporibus sequi. Est quibusdam repellat nisi esse, quasi minima accusamus quis inventore laudantium unde optio necessitatibus omnis harum.</p>
+        <p>Magni, unde deserunt. Repellendus, odit consequatur molestias quibusdam quia, fuga assumenda sapiente fugit rerum fugiat eaque tempora velit animi voluptatum, magnam expedita officiis cupiditate illum laboriosam doloribus. Quas, saepe eum.</p>
+        <p>Corrupti voluptatum, facere obcaecati voluptatem, laudantium architecto accusamus ipsa assumenda molestias maxime possimus eaque iure fugit vero quas ex provident eveniet! Eligendi, ab? Tenetur dicta, delectus fuga ducimus dolor doloremque?</p>
+        <p>Alias architecto, ipsam quos repellat fuga maxime expedita porro inventore quis voluptates distinctio provident molestiae facilis numquam aliquid dignissimos, laborum debitis id! Ducimus sit totam, facilis voluptatibus qui quod quam.</p>
+        <p>Expedita, sint amet consectetur officiis quae nulla inventore aspernatur ducimus quod nihil, esse architecto perferendis velit repellat harum necessitatibus id dicta libero a. Reprehenderit iure eveniet dolores eum praesentium voluptatem.</p>
+        <p>Qui culpa magnam quia corporis harum cupiditate cum tenetur nulla iste sapiente doloribus maxime, quisquam labore, voluptas libero quo dolor nam! Repellendus unde culpa quidem iusto odit necessitatibus, sint sunt!</p>
+        <p>Doloribus ex vel culpa molestias ducimus perferendis recusandae eaque odio quaerat harum dignissimos nam placeat temporibus quibusdam nihil consectetur, a distinctio numquam cumque fugiat provident iure soluta. Voluptatum, quae nisi.</p>
+        <p>Itaque cum fugit nisi accusantium, ab praesentium vel nostrum illo hic nesciunt doloremque beatae autem commodi cupiditate numquam fuga distinctio ut delectus error dicta asperiores a dignissimos? Doloribus, veniam voluptatem?</p>
+        <p>Dolorem nam a deleniti neque molestiae beatae magni sit officiis esse vero voluptas dolores vitae praesentium doloremque iure, nihil reiciendis alias fugit voluptatibus saepe architecto id commodi? Beatae, voluptatem inventore?</p>
     </div>
 </body>
 </html>

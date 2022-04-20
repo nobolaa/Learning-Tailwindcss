97712a6f15003e0e379c0951c7a6467494101e94
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 23:36:31 +0200

    Using flexbox and changing child items order depending on size screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,28 +10,11 @@
 </head>
 <body>
     <div class="container">
-        
         <div class="bg-gray-300 m-2 flex">
 
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1">1</div>
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2">2</div>
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2">3</div>
-
-        </div>
-        
-        <div class="bg-gray-300 m-2 flex">
-
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1">1</div>
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1">2</div>
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2">3</div>
-
-        </div>
-        
-        <div class="bg-gray-300 m-2 flex">
-
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1">1</div>
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1">2</div>
-            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1">3</div>
+            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1 lg:order-2">1</div>
+            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1 lg:order-1">2</div>
+            <div class="bg-gray-400 text-gray-800 px-4 py-2 m-2 flex-1 lg:order-3">3</div>
 
         </div>
     </div>

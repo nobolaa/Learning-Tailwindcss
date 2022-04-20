326e43a4eeb4799654caa4cfc09204077a3cf0a5
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 22:45:50 +0200

    Using position absolute with top/left/right/bottom and inset-x-0/inset-y-0 tailwindcss properties

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,16 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container bg-gray-300">
-        <div class="bg-gray-300 p-4 h-64 overflow-auto">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt similique iusto autem quo nulla ducimus corrupti voluptas ipsum amet illo! Ducimus, molestias provident? Enim reprehenderit commodi cum deserunt necessitatibus beatae?</p>
-            <p>Consequatur, accusantium? Doloribus alias aliquid iure eius quisquam corrupti incidunt suscipit inventore, quae, laboriosam odit aspernatur repellendus reprehenderit corporis est, nemo possimus minima. Alias iusto, consectetur iure porro blanditiis ut.</p>
-            <p>Quidem maiores dolorem sapiente qui! Quod, id ipsum nostrum distinctio maiores nisi commodi aperiam iste qui libero nulla et facilis, quidem rerum quis, neque cumque voluptas magni officia nobis! Esse.</p>
-            <p>Omnis voluptates ipsa sunt error eius. Commodi sed officiis quidem minima repellendus molestiae aperiam debitis, ipsa aliquam hic veritatis dolorem culpa nesciunt sapiente voluptas consequatur iste eveniet qui ea quo!</p>
-            <p>Inventore esse ipsum ea et, voluptate laudantium molestias sint ipsa dolor cumque, distinctio illum totam accusamus ullam dignissimos, voluptates non quibusdam eum. Nulla nam quasi, nemo autem maxime hic? Eum!</p>
-            <p>Harum assumenda hic deleniti dicta vitae cupiditate, voluptates, dolores magnam aliquam fugit natus quam repellat totam. Similique dolore neque voluptatum, aliquid esse mollitia soluta. Quos, vitae! Reprehenderit praesentium enim eius.</p>
-            <p>Et esse modi exercitationem nulla cupiditate a officia necessitatibus alias voluptatibus hic voluptatum, libero explicabo? Rerum dolore dolorem rem explicabo quas odit animi sapiente. Perferendis in molestias blanditiis odit nemo.</p>
-            <p>Tenetur atque, voluptatum maxime sunt laboriosam nemo dolores quibusdam sint provident id numquam nesciunt quis harum facilis iure cupiditate tempore, saepe aperiam maiores illum dolorum! Omnis culpa quis blanditiis incidunt.</p>
-            <p>Cumque voluptate, sit soluta atque placeat minus asperiores sed unde vitae, nobis qui dignissimos tempore, optio expedita perspiciatis. Beatae consequuntur voluptatem incidunt dicta quos quam quisquam quis error sint quasi?</p>
+    <div class="container mt-4">
+        <div class="bg-gray-300 p-4">
+            <div class="bg-gray-400 p-4">
+                <div class="bg-blue-400 p-4">
+           
+                </div>
+                <div class="bg-blue-600 p-4 absolute top-0 inset-x-0">
+           
+                </div>
+            </div>
         </div>
      </div>  
 </body>

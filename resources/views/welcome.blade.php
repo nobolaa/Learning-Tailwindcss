5b02584a5b11800d6716f2e1b12d2f21026ae130
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 17:27:42 +0200

    Using background as image.     - repeating image     - no repeating image     - borders

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Document</title>
+    <style>
+        .imagen{
+            background-image: url("{{asset('img/tailwind-intellisense.jpg')}}");
+            height: 400px;
+            width: 400px;
+            margin: 0 auto;
+        }
+    </style>
+
 </head>
 <body>
     <div class="container">
-        <h1 class="text-xl font-bold text-left md:text-center lg:text-right underline uppercase">Este es el titulo</h1>
-        <p class="text-red-700 hover:text-opacity-75">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores, consectetur molestiae officiis, perferendis asperiores sapiente iusto ad suscipit autem quis sit eveniet odio quibusdam tempora delectus ducimus odit adipisci rem?</p>
-        <ul class="list-disc list-inside">
-            <li class="line-through">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus libero asperiores explicabo sit magni placeat provident hic itaque, voluptas dolores animi ipsam sequi laboriosam vero doloribus laborum autem nisi repellendus.</li>
-            <li>Accusamus quod dicta perferendis tenetur iure, inventore cumque maiores doloribus sapiente, minima expedita nostrum quis voluptatem natus. Nemo, porro deleniti natus maxime esse aspernatur illum. Corporis deserunt ex voluptas at.</li>
-            <li>Accusamus rerum dignissimos, at sed nisi dolorem deleniti sequi exercitationem incidunt eveniet quae ex accusantium perspiciatis, tenetur, odit laboriosam culpa. Excepturi aspernatur explicabo ut enim ipsum accusantium assumenda aperiam quaerat!</li>
-            <li class="capitalize">Tempore voluptatibus recusandae, doloribus perspiciatis sint dolores beatae perferendis fuga deserunt odio amet quis illum nesciunt sed sunt unde explicabo, temporibus delectus totam voluptatum voluptas quaerat ipsa blanditiis minus! Quam.</li>
-            <li class="lowercase">Recusandae autem eaque ea in cumque consequuntur aspernatur minima omnis. Voluptatum sequi dolor nesciunt unde rem, earum doloremque eaque hic commodi! A, quisquam neque consequatur consequuntur autem dolore? Velit, magni!</li>
-        </ul>
+        <h1 class="text-center text-3xl font-bold mb-3">Background</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta adipisci voluptatem pariatur at architecto, ratione saepe cumque dolores dolorum porro, consectetur illo necessitatibus, ipsa vitae possimus minima provident nobis recusandae.</p>
+        <div class="imagen bg-contain bg-no-repeat border-8 border-blue-600"></div>
     </div>
 </body>
 </html>

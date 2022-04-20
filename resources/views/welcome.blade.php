4aa927c12a63647f96312eecdef6dd053dafe709
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 22:34:50 +0200

    Using object-cover and object-top to fill the div with an image from the top of the image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,21 +9,8 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
-
-        <img class="float-left w-1/5" src="{{ asset('img/tailwind2.png.') }}" alt="">
-        <img class="float-right w-1/3" src="{{ asset('img/tailwind-intellisense.jpg.') }}" alt="">
-
-        <p class="mb-2 clear-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus necessitatibus sit rerum, eveniet exercitationem porro quibusdam dignissimos iste perferendis deleniti. Molestiae ratione dolore veniam officiis, repudiandae debitis magni asperiores nostrum?</p>
-        <p class="mb-2">Natus itaque nihil ex, illum repellat assumenda est, atque consequatur, obcaecati a recusandae expedita soluta quidem unde voluptatum eveniet consequuntur! Placeat in cum doloribus incidunt veniam sint impedit, aperiam nobis?</p>
-        <p class="mb-2">Deleniti ratione dignissimos alias, asperiores repellendus eaque saepe ut ex similique officiis harum amet, iusto at quas perferendis eius totam quo ea mollitia, debitis impedit vero necessitatibus. Tenetur, neque autem.</p>
-        <p class="mb-2">Dolorem fugiat inventore recusandae dignissimos dolores praesentium, iste blanditiis, nemo maiores adipisci eum velit quos officia! Fugit delectus quaerat rerum recusandae nobis debitis dolores illum aspernatur eius, hic placeat quisquam?</p>
-        <p class="mb-2 clear-both">Consequuntur quasi perferendis nesciunt nihil facilis cupiditate vero assumenda deleniti voluptatem impedit quaerat nulla tempora, illo laudantium, quam esse dolor dignissimos veniam aspernatur quisquam fugiat delectus. Itaque aut vel molestiae.</p>
-        <p class="mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus necessitatibus sit rerum, eveniet exercitationem porro quibusdam dignissimos iste perferendis deleniti. Molestiae ratione dolore veniam officiis, repudiandae debitis magni asperiores nostrum?</p>
-        <p class="mb-2">Natus itaque nihil ex, illum repellat assumenda est, atque consequatur, obcaecati a recusandae expedita soluta quidem unde voluptatum eveniet consequuntur! Placeat in cum doloribus incidunt veniam sint impedit, aperiam nobis?</p>
-        <p class="mb-2">Deleniti ratione dignissimos alias, asperiores repellendus eaque saepe ut ex similique officiis harum amet, iusto at quas perferendis eius totam quo ea mollitia, debitis impedit vero necessitatibus. Tenetur, neque autem.</p>
-        <p class="mb-2">Dolorem fugiat inventore recusandae dignissimos dolores praesentium, iste blanditiis, nemo maiores adipisci eum velit quos officia! Fugit delectus quaerat rerum recusandae nobis debitis dolores illum aspernatur eius, hic placeat quisquam?</p>
-        <p class="mb-2">Consequuntur quasi perferendis nesciunt nihil facilis cupiditate vero assumenda deleniti voluptatem impedit quaerat nulla tempora, illo laudantium, quam esse dolor dignissimos veniam aspernatur quisquam fugiat delectus. Itaque aut vel molestiae.</p>
-    </div>  
+    <div class="container bg-gray-300">
+        <img class="w-full h-64 object-cover object-top" src="{{ asset('img/paisaje.jpg') }}" alt="">
+     </div>  
 </body>
 </html>

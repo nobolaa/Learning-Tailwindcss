7b01d0fb44d9ed6af166240a6774b6fe0e5901db
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 20:04:15 +0200

    Our table now has items separated on mobile, small and medium screens. Also declared specific width for each column

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,25 +11,29 @@
 <body>
 
     <div class="container">
-        <table class="table">
+        <table class="table w-full border-separate lg:border-collapse table-fixed">
             <thead>
                 <tr>
-                    <th>Pais</th>
-                    <th>Ciudad</th>
+                    <th class="w-1/4">Pais</th>
+                    <th class="w-1/4">Ciudad</th>
+                    <th class="w-2/4">Descripcion</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>Perú</td>
                     <td>Lima</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit dolorum eveniet amet iste, ratione, a facilis mollitia tenetur, beatae odit culpa. Culpa recusandae aspernatur commodi, accusamus ad enim fugiat saepe!</td>
                 </tr>
                 <tr>
                     <td>Colombia</td>
                     <td>Bogota</td>
+                    <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias ipsum eum alias illo animi amet modi ab expedita neque, harum nam minus culpa perferendis necessitatibus aut quaerat excepturi quod. Iste.</td>
                 </tr>
                 <tr>
                     <td>España</td>
                     <td>Madrid</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum blanditiis delectus nihil quisquam debitis enim perspiciatis quibusdam facere aliquam doloribus, asperiores recusandae quam voluptates ipsum soluta doloremque iure placeat pariatur.</td>
                 </tr>
             </tbody>
         </table>  

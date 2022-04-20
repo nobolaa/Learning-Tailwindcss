eb68ec3eb740987c93dd2fe61d4ab65ba1bbec4f
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 22:54:57 +0200

    Using position fixed for nav and sidebar, alongside z index to put nav above sidebar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,17 +9,32 @@
     <title>Document</title>
 </head>
 <body>
-    <div class="container mt-4">
-        <div class="bg-gray-300 p-4 relative">
-            <div class="bg-gray-400 p-4 relative">
-                <div class="bg-blue-400 p-4">
-           
-                </div>
-                <div class="bg-blue-600 p-4 absolute top-0 inset-x-0">
-           
-                </div>
-            </div>
-        </div>
-     </div>  
+    
+    <nav class="bg-blue-300 h-16 fixed inset-x-0 z-50">
+
+    </nav>
+
+    <aside class="w-64 bg-gray-800 fixed inset-y-0 Z-40">
+
+    </aside>
+ 
+    <div class="container pt-16">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut recusandae enim rem excepturi eaque placeat, aliquam accusamus perferendis molestias cumque ipsam dolor corporis, itaque, quisquam sapiente quos eligendi repellat laborum?</p>
+        <p>Rerum odio non enim provident architecto libero natus, minus eius! Doloribus illo molestias, sit magnam provident aperiam soluta! Qui vero sint eius eaque nesciunt enim in consectetur vitae, error repellendus?</p>
+        <p>Molestias eum aut voluptate sequi atque dolorem distinctio quidem laudantium, iusto debitis dolorum, vel amet! Voluptas quidem, maiores libero facere voluptate beatae ex voluptatum quam voluptatem. Doloremque, vero rem? Assumenda.</p>
+        <p>Dolorem non, expedita quaerat maxime cumque rerum sunt aliquam distinctio sint obcaecati in mollitia totam molestias iure inventore? Delectus alias molestias modi maxime ex, laborum nihil fugit et accusantium beatae.</p>
+        <p>Iste qui amet, voluptate sunt fuga, ullam similique numquam pariatur quod molestias laborum est corrupti, vero impedit. Laboriosam nihil corrupti quasi excepturi delectus repudiandae inventore optio molestias aliquid, reprehenderit soluta.</p>
+        <p>In rerum veniam nisi voluptatem minima reiciendis nihil commodi libero molestias beatae ea, officiis aliquid. Eum ipsum, earum reiciendis aliquid natus molestiae unde consectetur sunt vitae veniam perspiciatis error maiores?</p>
+        <p>Dolores, fugit sint velit minus natus ab magni placeat aspernatur repellat incidunt, quidem eius qui alias autem maiores pariatur impedit assumenda. Vitae corrupti amet pariatur autem quidem! Eaque, dignissimos est.</p>
+        <p>Odit totam ex aut voluptates cupiditate voluptate laudantium velit vel. Autem excepturi tempore, alias neque, quisquam ipsa praesentium, consectetur corporis cum facilis maxime blanditiis! Dolores a quod harum voluptas voluptate.</p>
+        <p>In veniam dolore sapiente tempore veritatis modi laboriosam provident sequi mollitia cum asperiores perspiciatis error temporibus recusandae totam dolorem, cumque minima? Iste eligendi totam odit natus excepturi inventore vel eaque.</p>
+        <p>Illum provident in, consequatur aliquid impedit voluptatibus aliquam sunt veniam nobis libero! Delectus explicabo architecto unde molestias repellendus recusandae pariatur ratione ea obcaecati aperiam reiciendis, voluptatem, nostrum, iure eligendi? Explicabo.</p>
+        <p>Eius, doloribus ut, eveniet magni, dolorem animi recusandae officiis architecto repellat omnis ab autem assumenda doloremque iste itaque quasi? Maxime eum optio non unde sequi libero consectetur, laborum repellendus assumenda!</p>
+        <p>Modi quo nulla doloribus exercitationem velit eius dicta voluptate, qui quas aut illo facere assumenda sit debitis necessitatibus adipisci ex ipsa iusto eveniet, rem ut, praesentium numquam provident laboriosam? Reprehenderit?</p>
+        <p>Accusamus minus culpa earum maiores fugiat officia fuga tempora quibusdam provident nobis illo, sint ad modi commodi est nihil perferendis rerum dolorem! Aliquid, necessitatibus? Facilis quia fuga ad natus optio.</p>
+        <p>Cupiditate, laboriosam dolor sunt iste commodi quo doloremque saepe eaque tempore magni corrupti culpa corporis earum! Adipisci deserunt magni illum natus eos voluptate tempora libero iusto, asperiores ex quo dolorum?</p>
+        <p>Quo consequatur corrupti iste! Consequatur, ex laboriosam. Officia natus laudantium ipsa ab provident at corrupti ut doloribus voluptatum accusantium itaque voluptas error commodi, excepturi, atque quaerat dicta deserunt incidunt dolorem?</p>
+        <p>Eos nam explicabo, sapiente fuga consequatur esse aperiam voluptates nihil nostrum ex voluptas omnis ut dolorum. Expedita ad, ea, corporis tenetur consequuntur maiores ab sint excepturi totam modi quaerat. Ut?</p>
+    </div>
 </body>
 </html>

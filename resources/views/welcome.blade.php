167832ca4cdac57195000c93b10a6eea6656b147
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 19:25:56 +0200

    Using divide-x and divide-y to add dividing lines

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,5 +11,21 @@
     <div class="container pt-5">
         <div class="w-64 h-64 bg-gray-500 border-blue-800 border-8 border-dashed rounded-full"></div>
     </div>
-</body>
+
+    <div class="divide-y-8 divide-gray-600 divide-dashed">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores rerum nam ratione ab possimus magni temporibus perferendis consequatur, perspiciatis rem voluptate veniam aliquam magnam blanditiis doloremque ut repudiandae at?</p>
+        <p>Cumque repellat provident consectetur animi, nemo ex voluptatum commodi obcaecati enim eos eligendi corrupti assumenda quam perspiciatis a dolores? Deleniti architecto error eveniet atque non deserunt esse! Est, atque numquam.</p>
+        <p>Optio explicabo omnis aperiam numquam, ipsum nihil eveniet suscipit ut iste vel. Expedita dolores aperiam maxime rem voluptas totam tempore quae facilis eaque, ipsam, quas quos minima atque quisquam consequuntur.</p>
+        <p>Molestiae delectus placeat illo quae beatae ratione commodi ducimus aspernatur reprehenderit voluptatum officiis quasi vel libero adipisci non, expedita reiciendis ipsa repellendus sint. Porro ducimus tenetur vitae amet, necessitatibus architecto.</p>
+        <p>Necessitatibus praesentium accusamus, velit vitae, optio a deserunt explicabo dolores placeat officiis nemo molestiae temporibus perspiciatis mollitia corrupti! Molestias eius cupiditate excepturi possimus nulla doloribus adipisci reiciendis repellat enim non.</p>
+    </div>
+
+    <nav class="divide-x-2 divide-blue-600">
+        <a href="" class="pl-1">Link 1</a>
+        <a href="" class="pl-1">Link 2</a>
+        <a href="" class="pl-1">Link 3</a>
+        <a href="" class="pl-1">Link 4</a>
+        <a href="" class="pl-1">Link 5</a>
+    </nav>
+    </body>
 </html>

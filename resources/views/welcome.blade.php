84c101deaf269330990efeb7e78ebdabf1770677
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-20 19:49:49 +0200

    Moved styles of our table to estilos.css and imported it at app.css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,30 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
 
     <div class="container">
-        <table>
+        <table class="table">
             <thead>
                 <tr>
-                    <th class="border border-gray-400 px-4 py-2 text-gray-800">Pais</th>
-                    <th class="border border-gray-400 px-4 py-2 text-gray-800">Ciudad</th>
+                    <th>Pais</th>
+                    <th>Ciudad</th>
                 </tr>
             </thead>
             <tbody>
-                <tr class="bg-gray-200">
-                    <td class="border border-gray-400 px-4 py-2">Perú</td>
-                    <td class="border border-gray-400 px-4 py-2">Lima</td>
+                <tr>
+                    <td>Perú</td>
+                    <td>Lima</td>
                 </tr>
                 <tr>
-                    <td class="border border-gray-400 px-4 py-2">Colombia</td>
-                    <td class="border border-gray-400 px-4 py-2">Bogota</td>
+                    <td>Colombia</td>
+                    <td>Bogota</td>
                 </tr>
-                <tr class="bg-gray-200">
-                    <td class="border border-gray-400 px-4 py-2">España</td>
-                    <td class="border border-gray-400 px-4 py-2">Madrid</td>
+                <tr>
+                    <td>España</td>
+                    <td>Madrid</td>
                 </tr>
             </tbody>
         </table>  
